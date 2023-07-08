4d7c510ecfb935cb4a1adf22d2e6f6adcb57ef06
Ahmed Saqr <ahmedhassansaqr28@gmail.com>
2023-07-08 07:57:22 +0300

    settings screen from Ahmed

--- lib/cubit/HomeCubit/HomeCubit.dart
+++ lib/cubit/HomeCubit/HomeCubit.dart
@@ -6,7 +6,6 @@ import 'package:chat_app/moduls/chats/chats_screen.dart';
 import 'package:chat_app/moduls/feeds/feeds_screen.dart';
 import 'package:chat_app/moduls/users/user_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
