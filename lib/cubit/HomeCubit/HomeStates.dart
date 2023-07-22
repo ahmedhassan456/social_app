f6bd59737d391d8081e143ae3768922a9208c4fb
Ahmed Saqr <ahmedhassansaqr28@gmail.com>
2023-07-22 17:15:40 +0300

    ...

--- lib/cubit/HomeCubit/HomeStates.dart
+++ lib/cubit/HomeCubit/HomeStates.dart
@@ -1,30 +1,42 @@
 abstract class HomeStates{}
 class HomeInitialState extends HomeStates{}
 
-
+// get user info
 class HomeGetUserSuccessState extends HomeStates{}
 class HomeGetUserErrorState extends HomeStates{
   final String? error;
   HomeGetUserErrorState(this.error);
-
 }
 class HomeGetUserLoadingState extends HomeStates{}
 
+// Bottom Nav Bar
 class HomeChangeBottomNav extends HomeStates{}
 
+// new post
 class HomeNewPost extends HomeStates{}
 
+// image picker
 class HomeProfileImagePickedSuccess extends HomeStates{}
 class HomeProfileImagePickedError extends HomeStates{}
-
 class HomeCoverImagePickedSuccess extends HomeStates{}
 class HomeCoverImagePickedError extends HomeStates{}
 
+// upload images
 class HomeUploadProfileImageSuccess extends HomeStates{}
 class HomeUploadProfileImageError extends HomeStates{}
 class HomeUploadCoverImageSuccess extends HomeStates{}
 class HomeUploadCoverImageError extends HomeStates{}
-
 class HomeUpdateCoverImageError extends HomeStates{}
 
+// create post
 class HomeLoadingUpdateUserData extends HomeStates{}
+class HomeLoadingCreatePostState extends HomeStates{}
+class HomeSuccessCreatePostState extends HomeStates{}
+class HomeErrorCreatePostState extends HomeStates{}
+
+// get post image
+class HomePostImagePickedSuccessState extends HomeStates{}
+class HomePostImagePickedErrorState extends HomeStates{}
+
+// remove post image
+class HomeRemovePostImageState extends HomeStates{}
